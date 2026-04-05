34e6e558ec708da8bb80b7f45aa566cd5fb56380
Laurynas Valiunas <laurynasvaliunas@gmail.com>
2026-04-05 18:36:18 +0300

    auto: sync changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import "../global.css";
 import "../src/i18n";
 import i18n from "../src/i18n";
+import * as Sentry from "@sentry/react-native";
 import React, { useEffect, useState } from "react";
 import { View, ActivityIndicator } from "react-native";
 import { Stack } from "expo-router";
