3e679317e1074ad20f8d92d6eff3df934ddde733
Laurynas Valiunas <laurynasvaliunas@gmail.com>
2026-04-06 10:46:58 +0300

    auto: sync changes

--- app/(tabs)/profile/settings.tsx
+++ app/(tabs)/profile/settings.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { View, Text, ScrollView, TouchableOpacity } from "react-native";
 import { useRouter } from "expo-router";
 import {
