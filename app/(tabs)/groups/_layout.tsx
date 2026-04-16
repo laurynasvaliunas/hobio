62b19aa5f07f861a176fd5799ce867659c3cd9b9
Laurynas Valiunas <laurynasvaliunas@gmail.com>
2026-04-16 19:05:32 +0300

    auto: sync changes

--- app/(tabs)/groups/_layout.tsx
+++ app/(tabs)/groups/_layout.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Stack } from "expo-router";
+import { ErrorBoundary } from "../../../src/components/ErrorBoundary";
 
 export default function GroupsLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <ErrorBoundary scope="groups">
+      <Stack screenOptions={{ headerShown: false }} />
+    </ErrorBoundary>
+  );
 }
