ea7eb6a1b5ffc4ae4846f347ff30eb468d9095fb
Laurynas Valiunas <laurynasvaliunas@gmail.com>
2026-04-16 18:53:42 +0300

    auto: sync changes

--- app/modals/create-announcement.tsx
+++ app/modals/create-announcement.tsx
@@ -8,7 +8,6 @@ import {
   KeyboardAvoidingView,
   Platform,
   Alert,
-  ActivityIndicator,
 } from "react-native";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
